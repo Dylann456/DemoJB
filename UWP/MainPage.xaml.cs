32bf43fcff7187f158c18b1e50e31c18194a6fad
Dylann456 <dylan.raumel.ga@gmail.com>
2019-09-19 17:22:47 +0200

    commentaires jb

--- UWP/MainPage.xaml.cs
+++ UWP/MainPage.xaml.cs
@@ -21,10 +21,12 @@ namespace UWP
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
+
     {
         public MainPage()
         {
             this.InitializeComponent();
+            // com jb
         }
     }
 }
